3257f5e0de72b1b9bbb4953a28974d73e591a4f4
Lucas <lucas.galeano@estudiantes.utec.edu.uy>
2022-10-06 00:44:22 -0300

    Develop

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Nombre: Fernando');
+    res.send('Nombre: Lucas');
 });
 
 module.exports = router;
