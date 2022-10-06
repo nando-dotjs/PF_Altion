2dfbc9ef46d18b1629ba78d21ce6f0889d235c21
Lucas <lucas.galeano@estudiantes.utec.edu.uy>
2022-10-06 00:32:35 -0300

    Todo bien

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hola Lucas como estas');
+    res.send('Todo bien Fernando');
 });
 
 module.exports = router;
