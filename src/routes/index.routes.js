8d27f16bf778b801cbeb9e6997bc404ad90338d8
Fernando <fperez.personal@gmail.com>
2022-10-06 00:35:51 -0300

    Cómo estás?

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Todo bien Fer');
+    res.send('Cómo estás Lucas?');
 });
 
 module.exports = router;
