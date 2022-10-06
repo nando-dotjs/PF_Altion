d2b891ba5748322cc8d2e870043b52fa88324703
Fernando <fperez.personal@gmail.com>
2022-10-05 23:52:14 -0300

    Hola Lucas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('Hola Lucas');
 });
 
 module.exports = router;
