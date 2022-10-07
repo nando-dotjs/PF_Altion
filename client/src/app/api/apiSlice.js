47e7b5c3be3fda9352d9d17504f69737b04cd713
Lucas <lucas.galeano@estudiantes.utec.edu.uy>
2022-10-07 19:23:08 -0300

    Corrección Persistencia

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { setCredentials, logOut } from '../../features/auth/authSlice'
+import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:5000',
--- client/src/features/auth/Login.js
+++ client/src/features/auth/Login.js
@@ -4,7 +4,6 @@ import { useNavigate, Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { setCredentials } from './authSlice'
 import { useLoginMutation } from './authApiSlice'
-import usePersist from '../../hooks/usePersist'
 
 const Login = () => {
     const userRef = useRef()
@@ -12,12 +11,16 @@ const Login = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [errMsg, setErrMsg] = useState('')
-    const [persist, setPersist] = usePersist()
+    const [persist] = useState(JSON.parse(localStorage.getItem("persist")));
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
     const [login, { isLoading }] = useLoginMutation()
+
+    useEffect(() => {
+        localStorage.setItem("persist", true)
+    }, [persist])
 
     useEffect(() => {
         userRef.current.focus()
@@ -52,7 +55,6 @@ const Login = () => {
 
     const handleUserInput = (e) => setUsername(e.target.value)
     const handlePwdInput = (e) => setPassword(e.target.value)
-    const handleToggle = () => setPersist(prev => !prev)
 
     const errClass = errMsg ? "errmsg" : "offscreen"
 
@@ -89,20 +91,7 @@ const Login = () => {
                         required
                     />
                     <button className="form__submit-button">Ingresar</button>
-
-                    <label htmlFor="persist" className="form__persist">
-                    <input
-                        type="checkbox"
-                        className="form__checkbox"
-                        id="persist"
-                        hidden
-                        onChange={handleToggle}
-                        checked={persist}
-                    />
-                        
-                    </label>
-
-                
+            
                 </form>
             </main>
             <footer>
