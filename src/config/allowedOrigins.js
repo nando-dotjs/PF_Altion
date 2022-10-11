f8d5a5364dd9b9c5a0cc268377bfbf3d41769fe6
Fernando <fperez.personal@gmail.com>
2022-10-10 22:50:28 -0300

    Cambios variables backend

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://unidosporlaclasificacion.onrender.com'
 ]
 
 module.exports = allowedOrigins
