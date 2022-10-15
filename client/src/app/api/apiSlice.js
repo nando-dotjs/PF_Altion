dc682c3057d2072fe4966652ebb9a178e39dd7e7
Lucas <lucas.galeano@estudiantes.utec.edu.uy>
2022-10-15 00:38:45 -0300

    Revive develop

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:5000',
+    baseUrl: 'http://localhost:5000', 
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
