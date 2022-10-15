64fc0b8b4461b785f8b8ba042a88d65a9c1147d0
Lucas <lucas.galeano@estudiantes.utec.edu.uy>
2022-10-14 22:58:53 -0300

    Se integra GetUser

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router()
 const usersController = require('../controllers/usersController')
 const verifyJWT = require('../middleware/verifyJWT')
 
-router.use(verifyJWT)
+// router.use(verifyJWT)
 
 router.route('/')
     .get(usersController.getAllUsers)                          
     .post(usersController.createNewUser)
     .patch(usersController.updateUser)
     .delete(usersController.deleteUser)
+
+router.route('/user')
+    .get(usersController.getUser)
 
 module.exports = router
