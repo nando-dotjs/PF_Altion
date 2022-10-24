96c739fe6ff92a4b0d7d4aa4f69cef7c3f011f6a
Lucas <lucas.galeano@estudiantes.utec.edu.uy>
2022-10-24 20:16:12 -0300

    192.168.1.100

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     'http://localhost:5000',
     'http://localhost:3000',
-    'http://192.168.1.11:5000',
-    'http://192.168.1.11:3000'
+    'http://192.168.1.100:5000',
+    'http://192.168.1.100:3000'
 ]
 
 module.exports = allowedOrigins
