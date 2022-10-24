b34c7a190f4a9b657a2bb2be31e3967d3bef24fc
Fernando <fperez.personal@gmail.com>
2022-10-23 22:02:21 -0300

    JWT

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const usersController = require('../controllers/usersController')
 const verifyJWT = require('../middleware/verifyJWT')
 
-// router.use(verifyJWT)
+router.use(verifyJWT)
 
 router.route('/')
     .get(usersController.getAllUsers)                          
