741a037ca3fa36e137a07af461229eb71497c14d
Matias Garcia <matias.100054@gmail.com>
2022-10-31 17:03:28 -0300

    Nuevas Suite y Sprint 2

---
+++ Tests/suites/suite7.py
@@ -0,0 +1,17 @@
+from funciones import AuxiliarFunctions 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+
+def registerCEVPoint():
+    print()
+
+def registerEMPPoint():
+    print()
+
+def modifyCEVPoint():
+    print()
+
+def modifyEMPPoint():
+    print()
