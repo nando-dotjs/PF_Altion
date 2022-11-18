cdafd0a1ca3c806a0189a2752ffd6f05877015de
Fernando <fperez.personal@gmail.com>
2022-11-18 20:22:48 -0300

    correciones

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {store} from './app/store'
 import {Provider} from 'react-redux'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-import { processResult } from 'immer/dist/internal';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
